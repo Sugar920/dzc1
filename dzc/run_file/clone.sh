6f4c0a7166e086331994139465e717aa3677c1e2
Sugar920 <121632458+Sugar920@users.noreply.github.com>
2026-01-06 20:47:29 +0800

    Update clone.sh

--- dzc/run_file/clone.sh
+++ dzc/run_file/clone.sh
@@ -2,3 +2,4 @@ cat /data/dzc/run_file/token.sh
 # git clone https://gh-proxy.com/https://github.com/Ascend/sglang.git
 git clone https://gh-proxy.com/https://github.com/Sugar920/sglang-project.git
 git clone https://gh-proxy.com/https://github.com/Sugar920/dzc.git
+git clone -b hhhh1252023-patch-1 https://github.com/shun8686/sglang.git
